00fc1ec4f19a95485b027bb05973e661fb38b156
Utkarsh Patel <itismeutkarsh@gmail.com>
2019-04-16 16:58:11 +0530

    Add composer.json; Add plugin file; Update Readme and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# login-with-google
+# WP-Google-Login
+
 Minimal plugin which allows WP user to login with google.
+
+
+## How to use it?
---
+++ wp-google-login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WordPress.org Plugin
+ * Plugin URI:  https://github.com/rtCamp/wp-google-login
+ * Description: Minimal plugin which allows WP user to login with google.
+ * Version:     0.1
+ * Author:      rtCamp
+ * Author URI:  https://rtcamp.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-google-login
+ */
