0a634cbe28e8fef2d46f5bf675e32b25aea2789a
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-10-20 23:29:39 +0530

    Remove build files

--- assets/build/js/block-button.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'efb47b40a292e71e');
--- assets/build/js/login.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '57f26158d9cbe8f4');
