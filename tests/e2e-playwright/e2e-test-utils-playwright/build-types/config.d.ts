528830fc407718823e22f063a7f38b9ccbdc2f62
pavanpatil1 <=>
2022-11-25 10:56:00 +0530

    Fix the CI issue and push the Build

--- tests/e2e-playwright/e2e-test-utils-playwright/build-types/config.d.ts
+++ tests/e2e-playwright/e2e-test-utils-playwright/build-types/config.d.ts
@@ -1,6 +1,6 @@
 declare const WP_ADMIN_USER: {
-    readonly username: "cool-wilson";
-    readonly password: "GOEZZtOOwpZsxrhPTz";
+    readonly username: "automation";
+    readonly password: "automation";
 };
 declare const WP_USERNAME: string, WP_PASSWORD: string, WP_BASE_URL: string;
 export { WP_ADMIN_USER, WP_USERNAME, WP_PASSWORD, WP_BASE_URL };
