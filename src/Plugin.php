ae5569015e4e57da727d9e4d25b9c5fd684a9bb0
Shah Shalin <shalinshh723@gmail.com>
2025-02-28 14:10:44 +0530

    refactor: remove redundant default values

--- src/Plugin.php
+++ src/Plugin.php
@@ -92,8 +92,8 @@ class Plugin {
 	 * @return void
 	 */
 	public function run(): void {
-		$this->path         = dirname( __DIR__, 1 );
-		$this->url          = plugin_dir_url( trailingslashit( dirname( __DIR__, 1 ) ) . 'login-with-google.php' );
+		$this->path         = dirname( __DIR__ );
+		$this->url          = plugin_dir_url( trailingslashit( dirname( __DIR__ ) ) . 'login-with-google.php' );
 		$this->template_dir = trailingslashit( $this->path ) . 'templates/';
 		$this->assets_dir   = trailingslashit( $this->path ) . 'assets/';
 
