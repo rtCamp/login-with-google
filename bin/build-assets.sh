5e3906bc5a514239ae432fe415187dd5daf3aa3a
Shah Shalin <shalinshh723@gmail.com>
2025-03-11 16:42:40 +0530

    feature: setup node version using actions/setup-node@v4

--- bin/build-assets.sh
+++ bin/build-assets.sh
@@ -7,14 +7,6 @@ set -e
 echo "Installing PHP dependencies..."
 composer install --no-dev
 
-# Use the correct Node.js version if NVM is available
-if command -v nvm &> /dev/null; then
-    echo "Using NVM to set Node.js version..."
-    nvm use
-else
-    echo "NVM is not installed. Skipping NVM setup."
-fi
-
 # Navigate to the assets directory
 cd ./assets || { echo "Error: ./assets directory not found"; exit 1; }
 
