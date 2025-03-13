4d60d4251b5e2e044f90f7545fff812996e77940
Shah Shalin <shalinshh723@gmail.com>
2025-03-13 19:15:19 +0530

    fix: ignored .git, .idea, .distignore files from plugin build

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -7,9 +7,20 @@ set -e
 dirname=$(dirname "$0")
 cd "$dirname/.." || exit 1
 
+# Create a temp directory
+mkdir "temp"
+
+# Copy everything from current directory to temp directory, exclude temp and files from .distignore
+rsync -av --exclude='temp' --exclude='assets/node_modules' --exclude='vendor' --exclude='.git' --exclude='.idea' ./ temp/
+
+# store the root directory path
+ROOT_DIR=$(pwd)
+
+# Change to temp directory
+cd temp || exit 1
+
 # Define environment variables
 PLUGIN_SLUG="login-with-google"
-ASSETS_DIR="wp-assets"
 
 # Extract version from readme.txt
 VERSION=$(grep -m1 "Stable tag:" readme.txt | awk '{print $NF}')
@@ -26,10 +37,13 @@ fi
 
 # Create release zip using .distignore for exclusions
 echo "Creating release zip..."
-mkdir -p ./release
-rsync -av --exclude-from=".distignore" ./ ./release/${PLUGIN_SLUG}-${VERSION}/
-cd ./release
+mkdir -p ../release
+rsync -av --exclude-from=".distignore" ./ ../release/${PLUGIN_SLUG}-${VERSION}/
+cd ../release
 zip -r "${PLUGIN_SLUG}-${VERSION}.zip" "${PLUGIN_SLUG}-${VERSION}"
 rm -rf "${PLUGIN_SLUG}-${VERSION}"
+
+# Clean up
+rm -rf "${ROOT_DIR}/temp"
 
 echo "Release zip created at ./release/${PLUGIN_SLUG}-${VERSION}.zip"
