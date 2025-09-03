58f9dc0090ab4a5ec32ca87eed45e3752ad274eb
Amaan Khan <amaanpathan5@gmail.com>
2025-09-03 22:06:09 +0530

    refactor: remove trailing commas

--- src/Utils/GoogleClient.php
+++ src/Utils/GoogleClient.php
@@ -254,7 +254,7 @@ class GoogleClient {
 		set_transient(
 			'rtcamp_google_oauth_state_' . $state_data['nonce'],
 			1,
-			apply_filters( 'rtcamp.google_login_oauth_state_expiration', 15 * MINUTE_IN_SECONDS ),
+			apply_filters( 'rtcamp.google_login_oauth_state_expiration', 15 * MINUTE_IN_SECONDS )
 		);
 
 		return base64_encode( wp_json_encode( $state_data ) );
